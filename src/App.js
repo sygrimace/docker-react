84a613713f5e5b3a0585d67b17be0ad8919e9671
Szymon Gramza <s.gramza@signalgroup.pl>
2021-04-15 20:01:21 +0200

    initial files

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          learn react I was changed again!
         </a>
       </header>
     </div>
